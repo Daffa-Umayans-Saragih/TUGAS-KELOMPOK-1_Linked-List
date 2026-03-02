9c9e5fa56495485cefcb78e65ba2f82d40731229
Daffa-Umayans-Saragih <daffaumayans2007@gmail.com>
2026-03-02 18:16:24 +0700

    Add files via upload

--- ADD_Last-Node-Node.cpp
+++ ADD_Last-Node-Node.cpp
@@ -6,17 +6,40 @@ using namespace std;
 // =====================
 struct Node {
     int data;
-    Node* next;
+    Node *next;
 };
 
 // Pointer head (awal linked list)
-Node* head = NULL;
+    Node *head = NULL;
+
+
+
+// =====================
+// Inisialisasi 3 Node Awal
+// =====================
+void NodeAwal() {
+    Node *node1 = new Node();
+    Node *node2 = new Node();
+    Node *node3 = new Node();
+
+    node1->data = 10;
+    node1->next = node2;
+
+    node2->data = 20;
+    node2->next = node3;
+
+    node3->data = 30;
+    node3->next = NULL;
+
+    first = node1;
+}
 
 // =====================
 // Fungsi untuk menampilkan isi linked list
 // =====================
+
 void display() {
-    Node* temp = head;
+    Node *temp = head;
 
     cout << "Isi Linked List: "<<endl;
     while (temp != NULL) {
@@ -27,36 +50,14 @@ void display() {
     
 }
 
-// =====================
-// Inisialisasi 3 Node Awal
-// =====================
-void createInitialNodes() {
-    Node* node1 = new Node();
-    Node* node2 = new Node();
-    Node* node3 = new Node();
-
-    node1->data = 10;
-    node2->data = 20;
-    node3->data = 30;
-
-    node1->next = node2;
-    node2->next = node3;
-    node3->next = NULL;
-
-    head = node1;
-}
-
-
 int main() {
 
-    createInitialNodes();
+    NodeAwal();
 
     cout << "Linked List Awal:\n";
     display();
 
 
-    cout << "\nSetelah Operasi:\n";
-    display();
 
     return 0;
 }
